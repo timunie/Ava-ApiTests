ecdc54db18b7f1ab8ec75f629a4f094efff2e57b
Tim <tim.uniewski@gmail.com>
2025-02-27 16:44:24 +0100

    update config

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -15,16 +15,16 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://timunie.github.io/Ava-ApiTests',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: 'Ava-ApiTests',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
-
+  organizationName: 'timunie', // Usually your GitHub org/user name.
+  projectName: 'Ava-ApiTests', // Usually your repo name.
+  trailingSlash: false,
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
 
